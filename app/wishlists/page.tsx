8cbced625704cd05fe82b0c8a95e90b359b330f6
imhillageartrain <hillaryodhiambo@geartrain.co>
2025-09-13 12:55:52 +0300

    refactor wishlist

--- app/wishlists/page.tsx
+++ app/wishlists/page.tsx
@@ -1,20 +1,17 @@
 "use client"
 import Navigation from '@/components/Navigation'
 import { Button } from '@/components/ui/button'
-import type { Product, WishList } from '@/lib/features/types'
+import type { WishList } from '@/lib/features/types'
 import { selectUserToken } from '@/lib/features/user/userSlice'
-import { getWishListsAsync, selectWishLists } from '@/lib/features/wishlist/wishlistSlice'
+import { getWishListsAsync } from '@/lib/features/wishlist/wishlistSlice'
 import { useAppDispatch, useAppSelector } from '@/lib/hooks'
-import Image from 'next/image'
-import Link from 'next/link'
 import React, { useEffect } from 'react'
-import { Trash } from 'lucide-react';
 import { useRouter } from 'next/navigation'
+import WishLists from '@/components/WishLists'
 
 export default function WishList() {
     const dispatch = useAppDispatch()
     const token = useAppSelector(selectUserToken)
-    const wishLists = useAppSelector(selectWishLists)
     const router = useRouter()
 
     useEffect(() => {
@@ -53,66 +50,7 @@ export default function WishList() {
                     <div className='mt-[2rem]'>
                         <p className='text-[0.875rem] md:text-[1.125rem]'>It's super simple to create a wishlist! When you're browsing and see something you love, click the heart icon to add that item to your list. You can even login and drop a little hint by sharing your wish list with your nearest and dearest!</p>
 
-                        <div className='mt-[2rem] py-[2rem]'>
-                            <div>
-                                {wishLists?.map((wishlist: WishList, index: number) => {
-                                    return <div key={index.toString()} className="">
-                                        <div>
-                                            <h2 className='font-bold text-lg'>{wishlist?.name}</h2>
-                                        </div>
-                                        <div className='mt-[1.5rem] grid grid-cols-2 gap-x-[0.5rem] gap-y-9 md:gap-x-6 lg:gap-x-[2rem] sm:grid-cols-2 md:grid-cols-3'>
-                                            {
-                                                wishlist?.products.map((product: Product, index: number) => {
-                                                    return <Link key={index?.toString()} href={`/product/${product?.product_id}`}>
-                                                        <div className="overflow-hidden flex flex-col justify-center items-center">
-                                                            {
-                                                                product && product?.urls !== undefined && product?.urls.length > 0 && <div className="relative w-full h-auto min-h-[15.5rem] sm:min-h-[21rem] md:min-h-[21.875rem] lg:min-h-[25.875rem]">
-                                                                    <Image
-                                                                        src={product?.urls[0]?.url}
-                                                                        alt={product?.name}
-                                                                        fill
-                                                                        style={{ objectFit: "cover" }}
-                                                                        className="rounded-none lg:rounded-tl-lg lg:rounded-tr-lg h-full w-full"
-                                                                        priority
-                                                                    />
-                                                                </div>
-                                                            }
-
-                                                            <div className=" pb-3 flex flex-col gap-y-[0.5rem] mt-[0.75rem] w-full justify-center">
-                                                                <h3 className="text-[0.875rem] font-semibold">{product.name}</h3>
-                                                                <p className="text-[1.125rem] font-normal">{product.description}</p>
-                                                                <p className="text-[1.25rem] font-bold">KES {product.price}</p>
-
-                                                                <div className='flex justify-between items-center'>
-                                                                    <Button className="w-[5.3rem] md:w-[11rem] md:items-center rounded-[0.125rem] bg-[#AF52DE] h-[2rem] font-semibold flex gap-x-[0.25] md:gap-[0.75rem]">
-                                                                        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="19" viewBox="0 0 21 19" fill="none">
-                                                                            <path d="M9.1665 18.3008C9.99493 18.3008 10.6665 17.6292 10.6665 16.8008C10.6665 15.9724 9.99493 15.3008 9.1665 15.3008C8.33808 15.3008 7.6665 15.9724 7.6665 16.8008C7.6665 17.6292 8.33808 18.3008 9.1665 18.3008Z" fill="white" />
-                                                                            <path d="M16.1665 18.3008C16.9949 18.3008 17.6665 17.6292 17.6665 16.8008C17.6665 15.9724 16.9949 15.3008 16.1665 15.3008C15.3381 15.3008 14.6665 15.9724 14.6665 16.8008C14.6665 17.6292 15.3381 18.3008 16.1665 18.3008Z" fill="white" />
-                                                                            <path d="M11.6665 10.3008H13.6665V7.31081H16.6565V5.31081H13.6665V2.33081H11.6665V5.31081H8.67651V7.31081H11.6665V10.3008Z" fill="white" />
-                                                                            <path d="M8.6665 14.3008H16.6665C16.8679 14.3002 17.0645 14.2388 17.2305 14.1246C17.3964 14.0104 17.524 13.8487 17.5965 13.6608L20.4265 6.30079H18.2865L15.9765 12.3008H9.3365L4.8465 1.53079C4.69427 1.16585 4.43727 0.854225 4.10799 0.635297C3.77871 0.416369 3.39192 0.299962 2.9965 0.300786H0.666504V2.30079H2.9965L7.7465 13.6808C7.82152 13.8634 7.94892 14.0197 8.11264 14.1301C8.27636 14.2404 8.46908 14.2998 8.6665 14.3008Z" fill="white" />
-                                                                        </svg>
-
-                                                                        <span className='text-[0.875rem] md:hidden'>Cart</span>
-
-                                                                        <span className='text-[0.875rem] hidden md:block'>Add to cart</span>
-                                                                    </Button>
-
-                                                                    <Button className='bg-white border border-[#7A4597] h-[2rem] md:w-[5rem] text-[#7A4597] text-[0.875rem]'>
-                                                                        Remove
-                                                                    </Button>
-
-                                                                    <Trash className='text-red-400 mr-[1.25rem] md:hidden' />
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </Link>
-                                                })
-                                            }
-                                        </div>
-                                    </div>
-                                })}
-                            </div>
-                        </div>
+                        <WishLists />
                     </div>
                 </div>
             </div>
