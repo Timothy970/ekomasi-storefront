e6c0cf2732478dc6261d37f185e0f15c67fefab0
imhillageartrain <hillaryodhiambo@geartrain.co>
2025-09-15 08:07:04 +0300

    make some changes

--- app/checkout/member/page.tsx
+++ app/checkout/member/page.tsx
@@ -1,8 +1,21 @@
+"use client"
+import CartSummary from '@/components/CartSummary'
 import Navigation from '@/components/Navigation'
+import PersonalInformation from '@/components/PersonalInformation'
 import React from 'react'
 
 export default function MemberCheckout() {
   return (
-    <Navigation>MemberCheckout</Navigation>
+    <Navigation>
+      <div className='w-full px-[1rem] lg:px-[3rem] mx-auto max-w-[90rem]'>
+        <div className='w-full flex flex-col md:flex-row-reverse md:gap-x-[2rem] mt-[2] lg:mt-[2.5rem]'>
+          <div className='w-full'>
+            <CartSummary />
+          </div>
+
+          <PersonalInformation />
+        </div>
+      </div>
+    </Navigation>
   )
 }
--- components/GuestCartItems.tsx
+++ components/GuestCartItems.tsx
@@ -1,103 +1,37 @@
-import Image from 'next/image'
+import { selectCart } from '@/lib/features/cart/cartSlice'
+import { useAppSelector } from '@/lib/hooks'
 import React from 'react'
-
-const cart_items = [
-    {
-        "product_id": "p1",
-        "name": "Cozy Sofa",
-        "description": "A comfortable 3-seater fabric sofa.",
-        "sku": "SOFA-001",
-        "price": 499.99,
-        "category_id": "furniture",
-        "stock_quantity": 12,
-        "created_at": "2025-09-10T12:00:00Z",
-        'image': 'https://images.unsplash.com/photo-1584917865442-de89df76afd3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmFnfGVufDB8fDB8fHww'
-    },
-    {
-        "product_id": "p2",
-        "name": "Wireless Headphones",
-        "description": "Noise-cancelling over-ear headphones.",
-        "sku": "HEAD-123",
-        "price": 199.99,
-        "category_id": "electronics",
-        "stock_quantity": 35,
-        "created_at": "2025-09-10T12:05:00Z",
-        'image': 'https://images.unsplash.com/photo-1584917865442-de89df76afd3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmFnfGVufDB8fDB8fHww'
-    },
-    {
-        "product_id": "p3",
-        "name": "Standing Desk",
-        "description": "Adjustable height standing desk.",
-        "sku": "DESK-789",
-        "price": 299.99,
-        "category_id": "office",
-        "stock_quantity": 20,
-        "created_at": "2025-09-10T12:10:00Z",
-        'image': 'https://images.unsplash.com/photo-1584917865442-de89df76afd3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmFnfGVufDB8fDB8fHww'
-    },
-    {
-        "product_id": "p4",
-        "name": "Running Shoes",
-        "description": "Lightweight and comfortable running shoes.",
-        "sku": "SHOE-456",
-        "price": 89.99,
-        "category_id": "sports",
-        "stock_quantity": 50,
-        "created_at": "2025-09-10T12:15:00Z",
-        'image': 'https://images.unsplash.com/photo-1584917865442-de89df76afd3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmFnfGVufDB8fDB8fHww'
-    },
-    {
-        "product_id": "p5",
-        "name": "Smartwatch",
-        "description": "Fitness tracking smartwatch with heart rate monitor.",
-        "sku": "WATCH-101",
-        "price": 149.99,
-        "category_id": "wearables",
-        "stock_quantity": 40,
-        "created_at": "2025-09-10T12:20:00Z",
-        'image': 'https://images.unsplash.com/photo-1584917865442-de89df76afd3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmFnfGVufDB8fDB8fHww'
-    }
-]
+import CartItemProducts from './CartItemProducts'
 
 export default function GuestCartItems() {
+    const cart = useAppSelector(selectCart)
+
     return (
-        <div className='w-full mt-[2rem] flex flex-col gap-y-[1.5rem] max-h-[70vh] overflow-y-scroll mb-[2rem]'>
-            {
-                cart_items?.map((item, index) => {
-                    return <div key={index?.toString()} className='gap-x-[1.5rem] w-full flex justify-between items-start py-[1.5rem] border-b border-[rgba(0,0,0,0.40)]'>
-                        <div className="w-[40%] h-full">
-                            <div className="relative w-full h-[10rem] md:h-[15rem]">
-                                <Image
-                                    src={item?.image}
-                                    alt="Example"
-                                    fill
-                                    className="w-full h-auto"
-                                />
-                            </div>
-                        </div>
+        <div className='flex flex-col w-full items-center justify-center'>
 
-                        <div className='w-[50%] flex flex-col gap-y-[0.5rem] font-semibold'>
-                            <span className='text-[1.25rem]'>KES 13,000</span>
-                            <span className='text-[0.875rem]'>Flip 4-in-1 Convertible Carrier - Leopard</span>
-
-                            <div className='flex flex-row w-full gap-x-[0.5rem]'>
-                                <span className='text-[0.875rem] font-light'>Brand:</span>
-                                <span className='text-[0.875rem] font-light'>Infantino</span>
-                            </div>
-
-                            <div className='flex flex-row w-full gap-x-[0.5rem]'>
-                                <span className='text-[0.875rem] font-light'>Color:</span>
-                                <span className='text-[0.875rem] font-light'>Grey</span>
-                            </div>
-
-                            <div className='flex flex-row w-full gap-x-[0.5rem]'>
-                                <span className='text-[0.875rem] font-light'>Size:</span>
-                                <span className='text-[0.875rem] font-light'>One Size</span>
-                            </div>
-                        </div>
+            {cart && cart.cart_items && cart.cart_items.length > 0 && (
+                <div className='flex flex-row justify-center items-center py-[1rem] gap-x-[0.38rem] border-b border-[rgba(0,0,0,0.40)] md:border-b-0 w-full md:hidden text-[0.75rem]'>
+                    <div className='flex items-center justify-start gap-x-[0.38rem] text-gray-600'>
+                        <span>{cart.cart_items.length}</span>
+                        <span>
+                            {cart.cart_items.length === 1 ? "Item" : "Items"}
+                        </span>
                     </div>
-                })
-            }
+
+                    <div className='flex items-center justify-start gap-x-[0.38rem]'>
+                        <span>KES | </span>
+                        <span className='font-semibold text-[1.25rem]'>{cart.total}</span>
+                    </div>
+                </div>
+            )}
+
+            <div className='w-full flex flex-col gap-y-[0.5rem]'>
+                {
+                    cart?.cart_items?.map((item, index) => {
+                        return <CartItemProducts hideBtns={true} key={index.toString()} item={item} />
+                    })
+                }
+            </div>
         </div>
     )
 }
