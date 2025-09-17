1f785c2eeba3a010478f8ce1d1ddc7bf0280255b
imhillageartrain <hillaryodhiambo@geartrain.co>
2025-09-17 18:22:32 +0300

    reduce the height

--- components/HeaderTopSlider.tsx
+++ components/HeaderTopSlider.tsx
@@ -23,7 +23,7 @@ export default function HeaderTopSlider({
             onMouseEnter={() => hoveredCategoryId && handleMouseEnter(hoveredCategoryId)}
             onMouseLeave={handleMouseLeave}
             className="w-full flex justify-center bg-white items-center absolute border-b top-[2.438rem] lg:px-[3rem] left-0 overflow-hidden transition-all duration-200 ease-in-out z-50"
-            style={{ height: hoveredCategoryId ? "23rem" : "0" }}
+            style={{ height: hoveredCategoryId ? "21rem" : "0" }}
         >
             <div className="max-w-[90rem] w-full grid grid-cols-3 gap-y-[0.69rem] gap-x-[2rem] h-full pt-[1rem]">
                 {subcategories.map((sub) => (
