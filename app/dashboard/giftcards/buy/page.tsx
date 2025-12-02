e4d24103a5234a89b750a3365ad12c8c74060c91
imhillageartrain <hillaryodhiambo@geartrain.co>
2025-12-02 20:54:55 +0300

    Make some updates

--- app/dashboard/giftcards/buy/page.tsx
+++ app/dashboard/giftcards/buy/page.tsx
@@ -188,7 +188,7 @@ export default function BuyGiftCards() {
     return (
         <Navigation>
             <DashboardLayout>
-                <div className='relative z-[12] w-full h-full my-[2rem] lg:my-[2.5rem] flex flex-col gap-y-[1.5rem] p-[2rem] shadow-md rounded-[1.25rem] mb-[1rem] md:mb-[2rem]'>
+                <div className='relative z-[12] w-full h-full my-[2rem] lg:my-[2.5rem] flex flex-col gap-y-[1.5rem] p-[2rem] shadow-md rounded-[1.25rem]'>
                     <svg className='absolute left-[7rem] top-[3rem] z-[9]' xmlns="http://www.w3.org/2000/svg" width="70" height="44" viewBox="0 0 70 44" fill="none">
                         <path d="M14.1428 41.3623C14.5382 41.3536 15.0227 41.3404 15.5865 41.3207C17.2985 41.2609 19.7401 41.1405 22.6451 40.8956C28.4674 40.4048 36.0973 39.4202 43.4293 37.4569C50.8081 35.4811 57.6454 32.5753 62.0948 28.387C64.2949 26.3161 65.8782 23.9631 66.6909 21.2741C67.5016 18.5915 67.5843 15.4474 66.5911 11.7263C65.3152 6.94596 62.9505 4.18069 60.2368 2.58181C57.4639 0.948069 54.1238 0.415593 50.7618 0.506712C47.4098 0.597592 44.1818 1.30429 41.7671 2.00391C40.5663 2.35183 39.5817 2.69351 38.9026 2.94589C38.5634 3.07193 38.3011 3.17539 38.1272 3.24587C38.0406 3.28097 37.976 3.30796 37.9348 3.32532C37.9141 3.334 37.8985 3.34078 37.8899 3.3445C37.886 3.34615 37.8835 3.34742 37.8822 3.34798L37.8814 3.34749L35.4006 4.42757L35.0972 1.74007L35.0977 1.73922C35.0976 1.73783 35.0971 1.73421 35.0965 1.72953L35.0628 1.47549C35.037 1.28961 34.9964 1.01054 34.9362 0.653673C34.8157 -0.0607892 34.6194 -1.08436 34.3211 -2.29843C33.7212 -4.73964 32.7219 -7.88854 31.1261 -10.8367C29.5253 -13.794 27.3942 -16.4201 24.5923 -18.0043C21.8501 -19.5547 18.2704 -20.2201 13.487 -18.9334C10.1931 -18.0474 7.81639 -16.5065 6.10617 -14.5401C4.38399 -12.5598 3.25128 -10.0465 2.61881 -7.09942C1.34183 -1.14859 2.16438 6.28737 3.95563 13.7297C5.736 21.1267 8.4183 28.3132 10.6674 33.669C11.7899 36.3421 12.8001 38.5487 13.5279 40.0837C13.7649 40.5837 13.9717 41.0123 14.1428 41.3623Z" stroke="#ADD1CB" strokeWidth="4" />
                     </svg>
@@ -386,15 +386,15 @@ export default function BuyGiftCards() {
                         </div>
                     </div>
 
-                    <div className='w-full flex items-start gap-x-[1rem] mt-[2rem]'>
+                    <div className='w-full flex items-start gap-x-[1rem] md:gap-x-[2rem] mt-[2rem]'>
                         <Button
-                            className='bg-[#E82989] text-white'
+                            className='bg-[#E82989] md:min-w-[10rem] text-white'
                             onClick={handleCreate}
                         >
                             Create
                         </Button>
 
-                        <Button className='bg-white border border-black text-black'>
+                        <Button className='bg-white border md:min-w-[10rem] border-black text-black'>
                             Cancel
                         </Button>
                     </div>
